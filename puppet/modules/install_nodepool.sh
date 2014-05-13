21e4c09810af910f1eabe3287600da55a63b95bd
Daniel Curran <daniel.curran@RACKSPACE.com>
2014-05-13 18:40:41 -0500

    separating nodepool

---
+++ puppet/modules/install_nodepool.sh
@@ -0,0 +1,65 @@
+#! /usr/bin/env bash
+
+# Sets up a master Jenkins server and associated machinery like
+# Zuul, JJB, Gearman, etc.
+
+set -e
+
+THIS_DIR=`pwd`
+
+DATA_REPO_INFO_FILE=$THIS_DIR/.data_repo_info
+DATA_PATH=$THIS_DIR/data
+OSEXT_PATH=$THIS_DIR/os-ext-testing
+OSEXT_REPO=https://github.com/dani4571/os-ext-testing
+PUPPET_MODULE_PATH="--modulepath=$OSEXT_PATH/puppet/modules:/root/config/modules:/etc/puppet/modules"
+
+
+if [[ ! -e $DATA_PATH ]]; then
+    echo "Enter the URI for the location of your config data repository. Example: https://github.com/jaypipes/os-ext-testing-data"
+    read data_repo_uri
+    if [[ "$data_repo_uri" == "" ]]; then
+        echo "Data repository is required to proceed. Exiting."
+        exit 1
+    fi
+    git clone $data_repo_uri $DATA_PATH
+fi
+
+if [[ "$PULL_LATEST_DATA_REPO" == "1" ]]; then
+    echo "Pulling latest data repo master."
+    cd $DATA_PATH; git checkout master && git pull; cd $THIS_DIR;
+fi
+
+# Pulling in variables from data repository
+. $DATA_PATH/nodepool_vars.sh
+
+# Validate there is a Nodepool SSH key pair in the data repository
+if [[ -z $NODEPOOL_SSH_KEY_PATH ]]; then
+    echo "Expected to find NODEPOOL_SSH_KEY_PATH in $DATA_PATH/vars.sh. Please correct. Exiting."
+    exit 1
+elif [[ ! -e "$DATA_PATH/$NODEPOOL_SSH_KEY_PATH" ]]; then
+    echo "Expected to find Jenkins SSH key pair at $DATA_PATH/$NODEPOOL_SSH_KEY_PATH, but wasn't found. Please correct. Exiting."
+    exit 1
+else
+    echo "Using Jenkins SSH key path: $DATA_PATH/$NODEPOOL_SSH_KEY_PATH"
+    NODEPOOL_SSH_PRIVATE_KEY_CONTENTS=`sudo cat $DATA_PATH/$NODEPOOL_SSH_KEY_PATH`
+    NODEPOOL_SSH_PUBLIC_KEY_CONTENTS=`sudo cat $DATA_PATH/$NODEPOOL_SSH_KEY_PATH.pub`
+fi
+
+
+
+PUBLISH_HOST=${PUBLISH_HOST:-localhost}
+
+APACHE_SSL_CERT_FILE=`cat $APACHE_SSL_ROOT_DIR/new.cert.cert`
+APACHE_SSL_KEY_FILE=`cat $APACHE_SSL_ROOT_DIR/new.cert.key`
+
+CLASS_ARGS="jenkins_api_key => '$JENKINS_API_KEY', "
+CLASS_ARGS="$CLASS_ARGS jenkins_api_user => '$JENKINS_API_USER', "
+CLASS_ARGS="$CLASS_ARGS rackspace_username => '$RACKSPACE_USERNAME', "
+CLASS_ARGS="$CLASS_ARGS rackspace_password => '$RACKSPACE_PASSWORD', "
+CLASS_ARGS="$CLASS_ARGS mysql_root_password => '$MYSQL_ROOT_PASSWORD', "
+CLASS_ARGS="$CLASS_ARGS mysql_password => '$MYSQL_PASSWORD', "
+CLASS_ARGS="$CLASS_ARGS publish_host => '$PUBLISH_HOST', "
+CLASS_ARGS="$CLASS_ARGS data_repo_dir => '$DATA_PATH', "
+CLASS_ARGS="$CLASS_ARGS url_pattern => '$URL_PATTERN', "
+
+sudo puppet apply --verbose $PUPPET_MODULE_PATH -e "class {'os_ext_testing::nodepool_test': $CLASS_ARGS }"
--- puppet/modules/os_ext_testing/manifests/master.pp
+++ puppet/modules/os_ext_testing/manifests/master.pp
@@ -21,21 +21,7 @@ class os_ext_testing::master (
   $upstream_gerrit_ssh_private_key = '',
   $upstream_gerrit_host_pub_key = '',
   $git_email = 'testing@myvendor.com',
-  $git_name = 'MyVendor Jenkins',
-  $mysql_root_password,
-  $mysql_password,
-  $nodepool_ssh_private_key = '',
-  $nodepool_template = 'nodepool-dan.yaml.erb',
-  $sysadmins = [],
-  $statsd_host = '',
-  $jenkins_api_user ='',
-  $jenkins_api_key ='',
-  $jenkins_credentials_id ='',
-  $rackspace_username ='',
-  $rackspace_password ='',
-  $rackspace_project ='',
-  $image_log_document_root = '/var/log/nodepool/image',
-  $enable_image_log_via_http = true,
+  $git_name = 'MyVendor Jenkins'
 ) {
   include os_ext_testing::base
   include apache
@@ -171,17 +157,6 @@ class os_ext_testing::master (
       password => '',
     }
 
-    #file { '/etc/jenkins_jobs/config':
-    #  ensure  => directory,
-    #  owner   => 'root',
-    #  group   => 'root',
-    #  mode    => '0755',
-    #  recurse => true,
-    #  force   => true,
-    #  source  => 'puppet:///modules/os_ext_testing/jenkins_job_builder/config',
-    #  notify  => Exec['jenkins_jobs_update'],
-    #}
-
     file { '/etc/jenkins_jobs/config':
       ensure  => directory,
       owner   => 'root',
@@ -297,38 +272,5 @@ class os_ext_testing::master (
     source  => 'puppet:///modules/openstack_project/zuul/scoreboard.html',
     require => File['/var/lib/recheckwatch'],
   }
-
-
-  class { '::nodepool':
-    mysql_root_password       => $mysql_root_password,
-    mysql_password            => $mysql_password,
-    nodepool_ssh_private_key  => $nodepool_ssh_private_key,
-    statsd_host               => $statsd_host,
-  }
-
-  file { '/etc/nodepool/nodepool.yaml':
-    ensure  => present,
-    owner   => 'nodepool',
-    group   => 'root',
-    mode    => '0400',
-    content => template("openstack_project/nodepool/${nodepool_template}"),
-    require => [
-      File['/etc/nodepool'],
-      User['nodepool'],
-    ],
-  }
-
-  file { '/etc/nodepool/scripts':
-    ensure  => directory,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0755',
-    recurse => true,
-    purge   => true,
-    force   => true,
-    require => File['/etc/nodepool'],
-    source  => 'puppet:///modules/openstack_project/nodepool/scripts',
-  }
-
 }
 
